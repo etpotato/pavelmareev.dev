29850e4ef4e44f3f97d6eaa248eb0e55a95a23ec
etpotato <mareev.pv@gmail.com>
2022-02-10 23:36:32 +0300

    Add matchmedia function

---
+++ src/scripts/utils/match-media.js
@@ -0,0 +1,16 @@
+export default (media) => (inCb, outCb) => {
+  const matchMedia = window.matchMedia(media);
+
+  const listener = (evt) => {
+    if (evt.matches) inCb && inCb();
+    else outCb && outCb();
+  };
+
+  listener(matchMedia);
+
+  if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+    matchMedia.addListener(listener);
+  } else {
+    matchMedia.addEventListener('change', listener);
+  }
+};
